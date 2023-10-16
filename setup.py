815c039522b8d5a5a9baa0474ec3e869cee8cc38
YuheCheng62 <cheng.yuhe62@gmail.com>
2023-10-16 16:44:21 -0700

    v2 first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(
+    name='DupCaller',
+    version='0.0.2',
+    description='A variant caller for barcoded DNA sequencing',
+    url='https://github.com/AlexandrovLab/DupCaller',
+    author='Yuhe Cheng',
+    author_email='yuc211@ucsd.edu',
+    packages=['DupCaller']
+    install_requires = ''
