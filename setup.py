fee50755bb81d0ddc9d60c50715d337251d37de9
YuheCheng62 <cheng.yuhe62@gmail.com>
2023-12-15 15:00:27 -0800

    Change setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import *
 
 setup(
     name="DupCaller",
@@ -13,5 +13,6 @@ setup(
         "DupCaller/DupCallerSummarize.py",
     ],
     packages=find_packages("src"),
+    package_dir={"": "src"},
     install_requires=["biopython>=1.78", "pysam>=0.19.0", "numpy>=1.21.5"],
 )
